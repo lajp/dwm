5c5d4b53686666c9221937352fe9d9cfb5a20315
Luukas Pörtfors <lajp@iki.fi>
2024-08-23 00:06:52 +0300

    feat: draw status on all monitors

--- dwm.c
+++ dwm.c
@@ -704,11 +704,8 @@ drawbar(Monitor *m)
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
-	if (!m->showbar)
-		return;
-
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
